e714272d22cf4464df99ced8b110ae3a07d0ac95
zhoupeng <287214764@qq.com>
2020-09-04 12:26:18 +0800

    jdk固定为1.8

--- src/main/java/com/zp/dubbo/MyDubboProviderApplication.java
+++ src/main/java/com/zp/dubbo/MyDubboProviderApplication.java
@@ -1,7 +1,5 @@
 package com.zp.dubbo;
 
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
